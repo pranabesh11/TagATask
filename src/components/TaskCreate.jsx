8a2edefe8ed056891578ae97bf13c684750c9e38
pranabesh11 <pranabeshpratiharjgm@gmail.com>
2024-12-06 12:54:38 +0530

    css change

--- src/components/TaskCreate.jsx
+++ src/components/TaskCreate.jsx
@@ -1436,7 +1436,7 @@ const closeModal = () => {
                           padding: "5px",
                           minHeight: "20px",
                           whiteSpace: "pre-wrap",
-                          fontSize: "20px",
+                          fontSize: "18px",
                           
                         }}
                         dangerouslySetInnerHTML={{ __html: taskDescription }}
